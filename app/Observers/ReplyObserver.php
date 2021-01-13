fa6728ef4f0c8daabc7f03133140048f7ddc9496
Lin <546594050@qq.com>
2021-01-13 20:58:00 +0800

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -1,34 +1,24 @@
 <?php
-
 namespace App\Observers;
-
 use App\Models\Reply;
-use App\Notifications\TopicReplied;
 // creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
+// saved, deleting, deleted, restoring, restored
+use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
-    {
-        // XSS 过滤
-        $reply->content = clean($reply->content, 'user_topic_body');
-    }
-
-    public function updating(Reply $reply)
-    {
-        //
-    }
-
     public function created(Reply $reply)
     {
-       // $reply->topic->increment('reply_count', 1);
-
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
-
+        $reply->topic->updateReplyCount();
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
     }
-
+    public function creating(Reply $reply)
+    {
+        $reply->content = clean($reply->content, 'user_topic_body');
+    }
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
+    }
 }
